59db341d0cfc3cab775f7e2da9d9f0c43ebba6c7
prabhupriya1681-alt <prabhupriya1681@gmail.com>
2025-08-30 15:50:03 +0530

    Update movieRoutes.js

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { protect, isAdmin } from "../middleware/auth.js";
-import { createMovie, getMovies, getMovie, updateMovie, deleteMovie } from "../controllers/movieController.js";
+// 'isAdmin' is removed from here because we are not using it anymore
+import { protect } from "../middleware/auth.js"; 
+import { 
+    createMovie, 
+    getMovies, 
+    getMovie, 
+    updateMovie, 
+    deleteMovie 
+} from "../controllers/movieController.js";
+
 const router = express.Router();
+
+// 'isAdmin' middleware is removed from the routes below to allow any logged-in user 
+// to create, update, or delete movies.
+
+router.post("/", protect, createMovie);
 router.get("/", getMovies);
 router.get("/:id", getMovie);
-router.post("/", protect, isAdmin, createMovie);
-router.put("/:id", protect, isAdmin, updateMovie);
-router.delete("/:id", protect, isAdmin, deleteMovie);
+router.put("/:id", protect, updateMovie);
+router.delete("/:id", protect, deleteMovie);
+
 export default router;
